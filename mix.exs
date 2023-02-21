a8e17de7b59ee56cc0c976429fc192d73c627d25
cognivore <66186054+cognivore@users.noreply.github.com>
2023-02-21 01:43:01 +0000

    Newest Elixir

--- mix.exs
+++ mix.exs
@@ -3,13 +3,13 @@ defmodule TypeClass.Mixfile do
 
   def project do
     [
-      app: :type_class,
+      app: :type_class_goo,
       aliases: aliases(),
       deps: deps(),
       preferred_cli_env: [espec: :test, quality: :test],
 
       # Versions
-      version: "1.2.8",
+      version: "1.21.0",
       elixir: "~> 1.11",
 
       # Docs
@@ -25,24 +25,22 @@ defmodule TypeClass.Mixfile do
   defp aliases do
     [
       quality: [
-          "test",
-          "espec",
-          "credo --strict"
-        ]
+        "test",
+        "espec",
+        "credo --strict"
+      ]
     ]
   end
 
   defp deps do
     [
       {:exceptional, "~> 2.1"},
-
-      {:espec,    "~> 1.8", only: :test, runtime: false},
-      {:credo,    "~> 1.5",  only: [:dev, :test], runtime: false},
-      {:inch_ex,  "~> 2.0",  only: [:dev, :docs, :test], runtime: false},
-
-      {:dialyxir, "~> 1.1",  only: :dev, runtime: false},
-      {:earmark,  "~> 1.4",  only: :dev, runtime: false},
-      {:ex_doc,   "~> 0.23", only: :dev, runtime: false},
+      {:espec, "~> 1.8", only: :test, runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:inch_ex, "~> 2.0", only: [:dev, :docs, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: :dev, runtime: false},
+      {:earmark, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false}
     ]
   end
 
@@ -59,7 +57,7 @@ defmodule TypeClass.Mixfile do
     [
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/witchcrafters/type_class"},
-      maintainers: ["Brooklyn Zelenka", "Steven Vandevelde"]
+      maintainers: ["doma.dev"]
     ]
   end
 end
